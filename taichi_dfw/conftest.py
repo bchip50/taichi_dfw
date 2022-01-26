614a4212d8e973f47e3c98aa26a29e91b36aa443
bchip50 <bill@chipmaninfo.com>
2022-01-26 15:51:21 -0600

    Cookie Cutter Initial commit.

---
+++ taichi_dfw/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from taichi_dfw.users.models import User
+from taichi_dfw.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
